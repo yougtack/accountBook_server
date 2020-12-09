1c0532611e1b6c56bb28cfb774272d2e6a541654
김영택 <yt.kim@how2marry.com>
2020-12-09 10:02:19 +0900

    Initial Commit

---
+++ src/test/java/com/web/account_book/AccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.account_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
