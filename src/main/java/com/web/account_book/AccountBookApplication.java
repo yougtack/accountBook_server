dfe8919af9c4230db21b0f0ba6a4f0673f5620bc
김영택 <yt.kim@how2marry.com>
2020-12-09 10:04:26 +0900

    init

--- src/main/java/com/web/account_book/AccountBookApplication.java
+++ src/main/java/com/web/account_book/AccountBookApplication.java
@@ -1,4 +1,4 @@
-package com.web.accountBook;
+package com.web.account_book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
