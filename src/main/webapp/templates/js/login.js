6cb93c5b17c7841b91792e0d0e7e6c9c939ea324
usser <ajsj9706@naver.com>
2020-12-15 13:13:49 +0900

    회원가입 구현

--- src/main/webapp/templates/js/login.js
+++ src/main/webapp/templates/js/login.js
@@ -5,13 +5,13 @@ function loginCheck() {
     let loginForm = document.getElementById('login_form');
 
     if(userName.value.trim().length <= 0 && passWord.value.trim().length <=0) {
-        inputLabel[0].textContent = '아이디를 입력해주세요.';
+        inputLabel[0].textContent = '유저이름을 입력해주세요.';
         inputLabel[0].style.color = 'red';
 
         inputLabel[1].innerHTML = '비밀번호를 입력해주세요.';
         inputLabel[1].style.color = 'red';
     } else if(userName.value.trim().length <= 0) {
-        inputLabel[0].textContent = '아이디를 입력해주세요.';
+        inputLabel[0].textContent = '유저이름을 입력해주세요.';
         inputLabel[0].style.color = 'red';
     } else if (passWord.value.trim().length <=0){
         inputLabel[1].innerHTML = '비밀번호를 입력해주세요.';
---
+++ src/main/webapp/templates/js/signup.js
@@ -0,0 +1,42 @@
+function signupCheck() {
+    let userName = document.getElementById('username');
+    let email = document.getElementById('email');
+    let passWord = document.getElementById('password');
+    let signupLabel = document.getElementsByClassName('signup_label');
+    let signupForm = document.getElementById('signup_form');
+
+    let cnt = 0;
+
+    (userName.value.trim().length <= 0) ? userNameLabel() : ++cnt;
+    (email.value.trim().length <= 0) ? emailLabel() : ++cnt;
+    (passWord.value.trim().length <= 0) ? passWordLabel() : ++cnt;
+
+    function userNameLabel() {
+        signupLabel[0].textContent = '유저이름을 입력해주세요.';
+        signupLabel[0].style.color = 'red';
+    }
+
+    function emailLabel() {
+        signupLabel[1].innerHTML = '이메일을 입력해주세요.';
+        signupLabel[1].style.color = 'red';
+    }
+
+    function passWordLabel() {
+        signupLabel[2].innerHTML = '비밀번호를 입력해주세요.';
+        signupLabel[2].style.color = 'red'
+    }
+
+    if(cnt === 3){
+        signupForm.submit();
+    }
+}
+
+document.getElementById('signup_btn').addEventListener('click', () => {
+    signupCheck();
+});
+
+document.getElementById('password').addEventListener('keypress', (e) => {
+    if(e.key === 'Enter'){
+        signupCheck();
+    }
+});
