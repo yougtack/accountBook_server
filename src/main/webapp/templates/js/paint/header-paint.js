08d4f81121319386c9c957bbba55c249c7063d6d
usser <ajsj9706@naver.com>
2020-12-21 13:54:56 +0900

    write 구현 중

--- src/main/webapp/templates/js/paint/header-paint.js
+++ src/main/webapp/templates/js/paint/header-paint.js
@@ -20,28 +20,25 @@ const USER = {
     xhttp.send();
 })();
 
-// let loginPaint =
-//     `<header class="header_wrapper">
-//             <div class="header_container">
-//                 <img id="nav_icon" class="nav_icon" src="image/list_icon.png" alt="list" />
-//                 <a class="header_logo font" href="write.html">가계부</a>
-//                 <div class="profile_content">
-//                     <img class="profile" src="image/info.gif" alt="profile" />
-//                     <p class="profile_name font">${USER.data.email}</p>
-//                 </div>
-//             </div>
-//     </header>`;
-
 let signupPaint =
     `<header class="header_wrapper">
             <div class="header_container">
                 <img id="nav_icon" class="nav_icon" src="image/list_icon.png" alt="list" />
-                <a class="header_logo font" href="write.html">가계부</a>
-                <div class="profile_content">
-                    <span class="header_login font" onclick="location.href = 'login.html'">로그인</span>
-                </div>
+                <a class="header_logo font" href="write.html">가계부</a>`;
+if (USER.data) {
+    signupPaint +=`
+        <div class="profile_content">
+            <img class="profile" src="image/info.gif" alt="profile"/>
+            <p class="profile_name font">${USER.data.username}</p>
+        </div>`;
+} else {
+    signupPaint +=`
+        <div class="profile_content">
+            <span class="header_login font" onclick="location.href = 'login.html'">로그인</span>
+        </div>`;
+}
+signupPaint +=`
             </div>
     </header>`;
-document.write(signupPaint);
 
-// (USER.data !== null) ? document.write(loginPaint) : document.write(signupPaint);
+document.write(signupPaint);
