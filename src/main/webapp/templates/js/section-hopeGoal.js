84d7dc2fdb52764c50c5466fc631ace96d780456
hyunmin <>
2021-01-15 17:44:18 +0900

    희망목표 구현 중

--- src/main/webapp/templates/js/section-hopeGoal.js
+++ src/main/webapp/templates/js/section-hopeGoal.js
@@ -33,9 +33,56 @@ window.addEventListener('load', () => {
 
     hopeGoalWrite.style.display = 'none';
     document.getElementById('hopeGoal_cnt').textContent = `내 희망목표(${hopeGoalData.data.length}/5)`;
+
+    for (value of hopeGoalData.data){
+        let hopeGoalTable = document.getElementById('hopeGoal_table');
+        let hopeGoalPercent = value.sum_cost * value.goal_cost / 100;
+
+        hopeGoalTable.innerHTML +=
+            `<div class="hopeGoal_list_div"><div class="hopeGoal_content_list" style="width: 35%; padding-left: 3px;">
+                <img class="hopeGoal_image" src="image/hopeGoal(1).png" alt="image" />
+                <div class="hopeGoal_click_div">
+                    <span class="hopeGoal_title font">${value.title}</span>
+                    <span class="hopeGoal_date font">
+                        ${value.start_date} ~ ${value.end_date}
+                    </span>
+                </div>
+            </div>
+            <div class="hopeGoal_content_list" style="width: 20%; text-align: center;">
+                <div class="hopeGoal_list_div">
+                    <span class="hopeGoal_list_text font">
+                        ${value.create_date}
+                    </span>
+                </div>
+            </div>
+            <div class="hopeGoal_content_list" style="width: 20%; text-align: right;">
+                <div class="hopeGoal_list_div">
+                    <span class="hopeGoal_list_text font" style="margin-right: 10px;">
+                        ${value.goal_cost.format()}
+                    </span>
+                </div>
+            </div>
+            <div class="hopeGoal_content_list" style="width: 25%;">
+                <div class="hopeGoal_list_div">
+                    <div class="hopeGoal_graph_frame hopeGoal_list_text">
+                        <div class="hopeGoal_graph" 
+                            style="width: ${(hopeGoalPercent >= 50) ? 100 : hopeGoalPercent}%">
+                        </div>
+                    </div>              
+                    <span class="hopeGoal_list_text hopeGoal_percent font">
+                        ${hopeGoalPercent}%
+                    </span>
+                </div>
+            </div>
+</div>`;
+    }
 });
 
 document.getElementById('hopeGoal_write_btn').addEventListener('click', () => {
+    if (hopeGoalData.data.length === 5){
+        alert("작성할 수 있는 글의 개수는 5개입니다.");
+        return false;
+    }
     hopeGoalList.style.display = 'none';
     hopeGoalWrite.style.display = 'block';
     document.getElementById('hopeGoal_start_date').value =
@@ -52,7 +99,7 @@ document.getElementById('hopeGoal_submit').addEventListener('click', () => {
         start_date: document.getElementById('hopeGoal_start_date').value,
         end_date: document.getElementById('hopeGoal_end_date').value,
         username: USER.data[0],
-        references_type: ""
+        references_type: document.getElementById('click_tag').textContent.split(":")[1]
     };
 
     if(value[0].value === ""){
@@ -88,3 +135,11 @@ document.getElementById('hopeGoal_submit').addEventListener('click', () => {
 document.getElementById('hopeGoal_cancel').addEventListener('click', () => {
    location.href = "hopeGoal.html";
 });
+
+let hopeGoalTag = document.getElementsByClassName('hopeGoal_tag');
+
+for (let i = 0; i < hopeGoalTag.length; i++){
+    hopeGoalTag[i].addEventListener('click', () => {
+       document.getElementById('click_tag').textContent = "선택태그 : " + hopeGoalTag[i].textContent;
+    });
+}
