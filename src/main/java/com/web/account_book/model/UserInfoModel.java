b29400cbbf68577791e206aebb6b69051b9028d3
youngtack <rladudxor59@naver.com>
2021-01-15 16:19:28 +0900

    ...

---
+++ src/main/java/com/web/account_book/model/UserInfoModel.java
@@ -0,0 +1,12 @@
+package com.web.account_book.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoModel {
+    String username;
+    String email;
+    String profile_path;
+}
