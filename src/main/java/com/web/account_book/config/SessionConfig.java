a4777ba66e0695a46aaf9ae87d6fe7ed4722c0bb
youngtack <rladudxor59@naver.com>
2021-02-09 16:01:24 +0900

    로그인 중복 삭제

--- src/main/java/com/web/account_book/config/SessionConfig.java
+++ src/main/java/com/web/account_book/config/SessionConfig.java
@@ -1,41 +1,41 @@
-package com.web.account_book.config;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-
-import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Map;
-
-@WebListener
-public class SessionConfig{
-    private static final Map<String, String> sessions = new HashMap<>();
-
-
-    public synchronized static boolean getSessionIdCheck(String type, String compareId, HttpSession session, HttpServletRequest request, HttpServletResponse response){
-        sessions.put(type, compareId);
-        for(String key : sessions.keySet()){
-            String value = sessions.get(key);
-            if(session.getAttribute("username").equals(value)){
-                removeSessionForDoubleLogin(value, request, response);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static void removeSessionForDoubleLogin(String userId, HttpServletRequest request, HttpServletResponse response){
-        System.out.println("remove userId : " + userId);
-        if(userId != null && userId.length() > 0){
-            System.out.println("로그인이 중복되어 로그아웃을 진행합니다.");
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-            if (auth != null){
-                new SecurityContextLogoutHandler().logout(request, response, auth);
-            }
-        }
-    }
-}
+//package com.web.account_book.config;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+//
+//import javax.servlet.annotation.WebListener;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@WebListener
+//public class SessionConfig{
+//    private static final Map<String, String> sessions = new HashMap<>();
+//
+//
+//    public synchronized static boolean getSessionIdCheck(String type, String compareId, HttpSession session, HttpServletRequest request, HttpServletResponse response){
+//        sessions.put(type, compareId);
+//        for(String key : sessions.keySet()){
+//            String value = sessions.get(key);
+//            if(session.getAttribute("username").equals(value)){
+//                removeSessionForDoubleLogin(value, request, response);
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    private static void removeSessionForDoubleLogin(String userId, HttpServletRequest request, HttpServletResponse response){
+//        System.out.println("remove userId : " + userId);
+//        if(userId != null && userId.length() > 0){
+//            System.out.println("로그인이 중복되어 로그아웃을 진행합니다.");
+//            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//            if (auth != null){
+//                new SecurityContextLogoutHandler().logout(request, response, auth);
+//            }
+//        }
+//    }
+//}
